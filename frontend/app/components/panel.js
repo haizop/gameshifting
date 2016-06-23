6072012d71f911e6a3c47b99c11aa0bbb9b88f99
“Haiz <haizop@gmail.com>
2016-06-23 12:03:32 -0400

    panel created

---
+++ frontend/app/components/panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dynamic from './dynamic';
+
+
+class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+}
+
+export default Panel;
