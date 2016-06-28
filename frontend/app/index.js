afdba7e65ec666818ca438dcf8ffe7c4ccb91274
“Haiz <haizop@gmail.com>
2016-06-28 11:17:31 -0400

    shuffling css

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/game';
-import './styles/bootstrap.min.css';
+import './styles/style.scss';
 
 
 ReactDOM.render(
